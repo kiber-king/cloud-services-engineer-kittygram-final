73f08347cf9e4f3c59cb89c6652775ef2c4e15f7
Gleb <gleb.kvasov26@mail.ru>
2026-01-19 03:15:15 +0300

    pushing first iteration

--- backend/cats/admin.py
+++ backend/cats/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Achievement, AchievementCat, Cat
+
+admin.site.register(Cat)
+admin.site.register(Achievement)
+admin.site.register(AchievementCat)
